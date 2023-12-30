f2b3ec5d014b3be6ab80367b891abfd00390c033
Danyang Liu <noelialiu2326@gmail.com>
2023-12-30 13:49:38 +1100

    connection

--- yelpcamp.js
+++ yelpcamp.js
@@ -19,7 +19,8 @@ const User = require('./models/user')
 const secret = process.env.SECRET || 'thisisasecret';
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL|| 'mongodb://127.0.0.1:27017/yelp-camp';;
+console.log(process.env.DB_URL);
 // 'mongodb://localhost:27017/yelpcamp'
 /* mongoose.connect(dbUrl, {
     useNewUrlParser: true,
